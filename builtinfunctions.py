64827a2a6f820a9191f0bb9542109667e838fee5
ZainNasrullah <zain.nasrullah.zn@gmail.com>
2016-09-28 22:58:15 -0400

    Cleaned up some files to follow PEP8

--- builtinfunctions.py
+++ builtinfunctions.py
@@ -1,9 +1,9 @@
-import random #requires using the module as random.randint()
+import random  # requires using the module as random.randint()
 # from random import *  (no need for random.method)
 
-import sys, os
+import sys
 
 for i in range(10):
-    print(random.randint(1,10))
+    print(random.randint(1, 10))
 
 sys.exit()
--- definefunctions.py
+++ definefunctions.py
@@ -1,12 +1,13 @@
-#defining functions
+# defining functions
 def hello(name):
     print("Hello " + name + "!")
 
+
 def plusOne(number):
-    return number+1
+    return number + 1
 
 
-hello('Zain') #passing Zain to the function hello
-print(plusOne(5)) #passing 5 to the plusOne function which returns a value
-print("Hello", end ='')
+hello('Zain')  # passing Zain to the function hello
+print(plusOne(5))  # passing 5 to the plusOne function which returns a value
+print("Hello", end='')
 print("World")
